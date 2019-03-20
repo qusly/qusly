5bd63598f75a79ec8a85a9e83bdc480b693f6e2c
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-03-20 23:20:34 +0100

    Add padding in AppBar

--- src/renderer/components/AppBar/styles.ts
+++ src/renderer/components/AppBar/styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { transparency, TOOLBAR_HEIGHT } from '~/renderer/constants';
 
 export const StyledAppBar = styled.div`
+  padding-left: 24px;
   border-bottom: 1px solid rgba(0, 0, 0, ${transparency.dividers});
 `;
 
