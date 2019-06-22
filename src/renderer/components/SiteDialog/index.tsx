000bf9e9ca53d4b0d727809824d24952f021b88c
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-06-22 19:01:26 +0200

    Add SiteDialog

--- src/renderer/components/SiteDialog/index.tsx
+++ src/renderer/components/SiteDialog/index.tsx
@@ -1,18 +1,22 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import { Dropdown, MenuItem } from 'wexond-ui';
+import { Dropdown, MenuItem, Textfield } from 'wexond-ui';
 
 import Dialog from '../Dialog';
 import store from '~/renderer/store';
+import { Form } from './styles';
 
 export default observer(() => {
   return (
     <Dialog title="Add new site" visible={store.overlay.content === 'add-site'}>
-      <Dropdown>
-        <MenuItem label="FTP" />
-        <MenuItem label="FTPS" />
-        <MenuItem label="SFTP" />
-      </Dropdown>
+      <Form>
+        <Dropdown defaultValue="SFTP">
+          <MenuItem label="FTP" />
+          <MenuItem label="FTPS" />
+          <MenuItem label="SFTP" />
+        </Dropdown>
+        <Textfield label="Hostname"  />
+      </Form>
     </Dialog>
   );
 });
---
+++ src/renderer/components/SiteDialog/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Form = styled.div`
+  padding-top: 8px;
+  display: flex;
+`;
