95cac7965b0d62bb82060820bf8d2a590de5de8c
Sential <sentialx@gmail.com>
2019-06-23 20:08:41 +0200

    feat: add title to FileTree

--- src/renderer/components/FileTree/index.tsx
+++ src/renderer/components/FileTree/index.tsx
@@ -3,12 +3,16 @@ import { observer } from 'mobx-react';
 
 import store from '~/renderer/store';
 import Item from '../FileTreeItem';
+import { Pagebar, PageTitle } from '../SitesManager/styles';
 
 export default observer(() => {
   const session = store.sessions.current;
 
   return (
     <React.Fragment>
+      <Pagebar>
+        <PageTitle>Explorer</PageTitle>
+      </Pagebar>
       {session &&
         session.tree.items.map(item => <Item key={item._id} data={item} />)}
     </React.Fragment>
