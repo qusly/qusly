356cd19fcdbe8000936024f44e8bb6229018904e
Sential <sentialx@gmail.com>
2019-06-23 14:40:42 +0200

    fix: react import

--- src/renderer/components/Titlebar/index.tsx
+++ src/renderer/components/Titlebar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { WindowsControls } from 'react-windows-controls';
 import { closeWindow, maximizeWindow, minimizeWindow } from '~/renderer/utils';
 import { StyledTitlebar, Handle, Title, Icon } from './style';
