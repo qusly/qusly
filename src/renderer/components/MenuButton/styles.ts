e9e148d8db84d8e433e8bfc6888e4fd95d1cf259
Sential <sentialx@gmail.com>
2019-06-23 19:34:55 +0200

    style: remove circle hover

--- src/renderer/components/MenuButton/styles.ts
+++ src/renderer/components/MenuButton/styles.ts
@@ -1,5 +1,5 @@
 import styled, { css } from 'styled-components';
-import { centerBoth, centerIcon, transparency } from 'wexond-ui';
+import { centerIcon, transparency } from 'wexond-ui';
 
 import { MENU_WIDTH } from '~/renderer/constants';
 
@@ -8,30 +8,16 @@ export const StyledButton = styled.div`
   height: ${MENU_WIDTH}px;
   cursor: pointer;
   position: relative;
-
-  &::before {
-    content: '';
-    display: block;
-    position: absolute;
-    width: 40px;
-    height: 40px;
-    background-color: #000;
-    opacity: 0;
-    border-radius: 100%;
-    transition: 0.15s opacity;
-    ${centerBoth()};
-  }
-
-  &:hover::before {
-    opacity: 0.04;
-  }
 `;
 
 export const Icon = styled.div`
   width: ${MENU_WIDTH}px;
   height: ${MENU_WIDTH}px;
-  transition: 0.15s opacity;
   ${centerIcon(24)};
+
+  &:hover {
+    opacity: 1;
+  }
 
   ${({ icon, selected }: { icon: any; selected: boolean }) => css`
     background-image: url(${icon});
