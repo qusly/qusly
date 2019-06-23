1f9030dab037d8c5941797bf358b96cbe7ad1cc6
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-06-23 15:55:05 +0200

    Add app icons

--- src/renderer/components/Titlebar/index.tsx
+++ src/renderer/components/Titlebar/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { WindowsControls } from 'react-windows-controls';
+
 import { closeWindow, maximizeWindow, minimizeWindow } from '~/renderer/utils';
 import { StyledTitlebar, Handle, Title, Icon } from './style';
 
