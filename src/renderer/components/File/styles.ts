3f69ee10dc79a0a295dbbbec171c87ea531cef66
Sential <sentialx@gmail.com>
2019-06-25 17:05:33 +0200

    style: add borders to selected files

--- src/renderer/components/File/styles.ts
+++ src/renderer/components/File/styles.ts
@@ -8,14 +8,17 @@ export const StyledFile = styled.div`
   align-items: center;
   flex-direction: column;
   padding: 0px 16px 8px 16px;
-  border: 1px solid transparent;
 
   ${({ selected }: { selected: boolean }) => css`
-    background-color: ${selected ? 'rgba(98, 0, 234, 0.12)' : 'none'};
+    background-color: ${selected ? 'rgba(98, 0, 234, 0.08)' : 'none'};
+
+    border: ${selected
+      ? `1px solid rgba(98, 0, 234, 0.12)`
+      : `1px solid transparent`};
 
     &:hover {
       background-color: ${selected
-        ? 'rgba(98, 0, 234, 0.16)'
+        ? 'rgba(98, 0, 234, 0.12)'
         : 'rgba(0, 0, 0, 0.08)'};
     }
   `}
