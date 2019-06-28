008044381c5b12ccddc9b6abe8066c9717593e1f
Sential <sentialx@gmail.com>
2019-06-28 17:14:44 +0200

    feat: add auto updater logic

--- src/renderer/components/Toolbar/index.tsx
+++ src/renderer/components/Toolbar/index.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
+import { observer } from 'mobx-react';
+import { ipcRenderer } from 'electron';
 
 import NavigationButtons from '../NavigationButtons';
 import PathView from '../PathView';
 import { StyledToolbar } from './styles';
+import ToolbarButton from '../ToolbarButton';
+import { icons } from '~/renderer/constants';
+import store from '~/renderer/store';
 
-export default () => {
+const onUpdateClick = () => {
+  ipcRenderer.send('update-install');
+};
+
+export default observer(() => {
   return (
     <StyledToolbar>
       <NavigationButtons />
       <PathView />
     </StyledToolbar>
   );
-};
+});
