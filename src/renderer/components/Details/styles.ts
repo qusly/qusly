d0e5d81d1911789e125e6fd17079e38dfa29cc8a
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-07-04 12:15:16 +0200

    feat: add close icon in details panel

--- src/renderer/components/Details/styles.ts
+++ src/renderer/components/Details/styles.ts
@@ -1,17 +1,17 @@
 import styled, { css } from 'styled-components';
-import { centerIcon } from 'wexond-ui';
+import { centerIcon, transparency } from 'wexond-ui';
+import { icons } from '~/renderer/constants';
 
 export const Header = styled.div`
   width: 100%;
   display: flex;
-  align-items: center;
   padding: 8px 24px;
-  margin-top: 8px;
+  align-items: flex-start;
 `;
 
 export const Icon = styled.div`
-  width: 24px;
-  height: 24px;
+  min-width: 24px;
+  min-height: 24px;
   ${centerIcon(24)}
 
   ${({ icon, opacity }: { icon: string, opacity: number }) => css`
@@ -23,4 +23,21 @@ export const Icon = styled.div`
 export const Name = styled.div`
   font-size: 18px;
   margin-left: 12px;
+  overflow: hidden;
+`;
+
+export const Close = styled.div`
+  min-width: 20px;
+  min-height: 20px;
+  padding-left: 8px;
+  margin-left: auto;
+  background-image: url(${icons.close});
+  cursor: pointer;
+  opacity: ${transparency.icons.inactive};
+  transition: 0.15s opacity;
+  ${centerIcon(20)};
+
+  &:hover {
+    opacity: 1;
+  }
 `;
