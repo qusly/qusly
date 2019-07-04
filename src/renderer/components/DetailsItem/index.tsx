f9939e8712510be5ec36a07914367186ca8d17e5
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-07-04 11:19:06 +0200

    feat: add DetailsItem

---
+++ src/renderer/components/DetailsItem/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+import { StyledItem, Property, Value } from './styles';
+
+export default () => {
+  return (
+    <StyledItem>
+      <Property>Property</Property>
+      <Value>Value</Value>
+    </StyledItem>
+  );
+};
---
+++ src/renderer/components/DetailsItem/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { robotoMedium } from 'wexond-ui';
+
+export const StyledItem = styled.div`
+  height: 48px;
+  padding: 16px;
+`;
+
+export const Property = styled.div`
+  font-size: 16px;
+  ${robotoMedium()};
+`;
+
+export const Value = styled.div`
+  font-size: 13px;
+  margin-top: 4px;
+`;
