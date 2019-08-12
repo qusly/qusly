22378fe1cd7fff56595384d3d5e6fdb94a8f5fa7
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-08-12 23:04:34 +0200

    feat: add transparency and icons constants

---
+++ src/renderer/app/constants/icons.ts
@@ -0,0 +1,6 @@
+export const icons = {
+  fileTree: require('~/renderer/resources/icons/file-tree.svg'),
+  sitesManager: require('~/renderer/resources/icons/sites-manager.svg'),
+  search: require('~/renderer/resources/icons/search.svg'),
+  transfer: require('~/renderer/resources/icons/transfer.svg'),
+}
--- src/renderer/app/constants/index.ts
+++ src/renderer/app/constants/index.ts
@@ -1,2 +1,4 @@
 export * from './design';
 export * from './tabs';
+export * from './design';
+export * from './transparency';
