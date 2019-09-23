95b2feb88633ba3b4413a5b93c0afa7644e83fb4
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-09-23 15:11:21 +0200

    refactor: sites store

--- src/interfaces/site.ts
+++ src/interfaces/site.ts
@@ -3,4 +3,5 @@ import { IConfig } from 'qusly-core';
 export interface ISite extends IConfig {
   _id?: string;
   title?: string;
+  date?: Date;
 }
--- src/renderer/app/store/sites.ts
+++ src/renderer/app/store/sites.ts
@@ -17,17 +17,7 @@ export class SitesStore {
 
   @action
   private async load() {
-    this.list = await this.db.get({} as any);
-  }
-
-  @action
-  public async add(site: ISite) {
-    const { password } = site;
-    const res = await this.db.insert({ ...site, password: '' });
-
-    this.list.push(res);
-
-    await setPassword(res._id, password);
+    this.list = await this.db.get({} as any); // TODO
   }
 
   @action
@@ -37,13 +27,25 @@ export class SitesStore {
   }
 
   @action
+  public async add(site: ISite) {
+    const { password } = site;
+    const res = await this.db.insert({ ...site, password: '', date: new Date() });
+
+    this.list.push(res);
+
+    await setPassword(res._id, password);
+  }
+
+  @action
   public async update(_id: string, changed: ISite) {
     const index = this.list.indexOf(this.list.find(r => r._id === _id));
+    const { password } = changed;
 
-    this.list[index] = { ...this.list[index], ...changed, password: '' };
+    changed = { ...changed, password: '' };
+    this.list[index] = { ...this.list[index], ...changed };
 
-    await this.db.update({ _id } as any, { ...changed }); // TODO
-    await setPassword(_id, changed.password);
+    await this.db.update({ _id } as any, changed); // TODO
+    await setPassword(_id, password);
   }
 
   @action
