efe6575160ec2c411b92c9fa838faa02b5fdefe0
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-10-07 18:22:50 +0200

    feat: add progressbar

--- src/renderer/app/components/TransferOverlay/index.tsx
+++ src/renderer/app/components/TransferOverlay/index.tsx
@@ -4,15 +4,18 @@ import { observer } from 'mobx-react-lite';
 import store from '~/renderer/app/store';
 import { Section } from './Section';
 import { StyledTransfer } from './style';
+import { Progressbar } from '~/renderer/components/Progressbar';
 
 export const TransferOverlay = observer(() => {
   if (store.activitybar.content !== 'transfer') return null;
 
   return (
     <StyledTransfer>
-      {store.transfer.sections.map(r => (
-        <Section key={r._id} data={r} />
-      ))}
+      <Progressbar value={50} />
     </StyledTransfer>
   );
 });
+
+/*{store.transfer.sections.map(r => (
+        <Section key={r._id} data={r} />
+      ))}*/
