b8f2d8ceae508d6626821a5617883353b632c671
Sential <sentialx@gmail.com>
2019-12-31 18:54:03 +0100

    fix: select Sites when there is no active session

--- src/renderer/app/store/activitybar.ts
+++ src/renderer/app/store/activitybar.ts
@@ -1,8 +1,19 @@
-import { observable } from 'mobx';
+import { observable, computed } from 'mobx';
+import store from '.';
 
 export type SidebarContent = 'explorer' | 'sites' | 'search' | 'transfer';
 
 export class ActivitybarStore {
   @observable
-  public content: SidebarContent = 'explorer';
+  public _content: SidebarContent = 'explorer';
+
+  public set content(value: SidebarContent) {
+    this._content = value;
+  }
+
+  @computed
+  public get content(): SidebarContent {
+    if (!store.sessions.current) return 'sites';
+    return this._content;
+  }
 }
