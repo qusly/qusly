6bce400449d5557faab6cae1302072b25798adb6
Mikoaj Palkiewicz <xnerhu@gmail.com>
2020-03-02 21:09:37 +0100

    feat: indicate file fetching by preloader in tab

--- src/renderer/app/components/Page/style.ts
+++ src/renderer/app/components/Page/style.ts
@@ -1,24 +1,12 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import { SelectionArea } from 'rectangle-selection';
 
 import { APPBAR_HEIGHT } from '../../constants';
-import { Preloader as StyledPreloader } from '~/renderer/components/Preloader';
-import { centerBoth } from '~/renderer/mixins';
 
 export const StyledPage = styled.div`
   width: 100%;
   height: calc(100% - ${APPBAR_HEIGHT}px);
   position: relative;
-`;
-
-const gridAnimation = keyframes`
-  0% {
-    opacity: 0;
-  }
-
-  100% {
-    opacity: 1;
-  }
 `;
 
 export const Grid = styled(SelectionArea)`
@@ -30,10 +18,4 @@ export const Grid = styled(SelectionArea)`
   grid-template-columns: repeat(auto-fill, minmax(96px, 1fr));
   grid-template-rows: repeat(auto-fill, 72px);
   overflow-y: auto;
-  animation: 0.1s ${gridAnimation};
-`;
-
-export const Preloader = styled(StyledPreloader)`
-  position: absolute;
-  ${centerBoth()};
 `;
