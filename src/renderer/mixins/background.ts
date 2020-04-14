0edd98c7c641abe1eb8d28474b2fa807aa587f23
xnerhu <xnerhu@gmail.com>
2020-04-14 20:18:20 +0200

    feat: improve blur effect

--- src/renderer/mixins/background.ts
+++ src/renderer/mixins/background.ts
@@ -3,7 +3,7 @@ import { css } from 'styled-components';
 import { CARD_SHADOW } from '../app/constants';
 
 export const blurBackground = css`
-  background-color: rgba(255, 255, 255, 0.7);
-  backdrop-filter: blur(10px);
+  background-color: rgba(0, 0, 0, 0.02);
+  backdrop-filter: blur(32px);
   box-shadow: ${CARD_SHADOW};
 `;
